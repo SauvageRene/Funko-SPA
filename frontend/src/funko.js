7a493f23801b7a1b77b34ab3c5e2692ce9422e59
Rene <renegarcia1864@gmail.com>
2021-07-26 13:39:52 -0500

    Funko HTML

--- frontend/src/funko.js
+++ frontend/src/funko.js
@@ -1,7 +1,31 @@
 class Funko {
-    constructor({name, image, series}) {
+    // remember all objects
+    static all = [];
+    static funkoContainer = document.getElementsByid('funkos-container');
+
+    constructor(name, image, series){
         this.name = name
         this.image = image
         this.series = series
+        this.element = document.createElement('li')
+        this.element = dataset.id = this.id
+        this.element.id = `funko-${this.id}`
+
+        Funko.all.push(this)
     }
-}
+    // Create the inner HTML for the element
+
+    funkoHTML(){
+        this.element.innerHTML += `
+        <div>
+            <h4>${this.name}</h4>
+            <p>${this.image}</p>
+            <p>${this.series}</p>
+        </div>`
+        return this.element
+    }
+    // append our element to the contact-container
+    renderToDom(){
+        Funko.funkoContainer.appendchild(funkoHTML())
+    }
+};
--- frontend/src/funkoServices.js
+++ frontend/src/funkoServices.js
@@ -1,5 +1,18 @@
 // Make the funko calls and services 
 
 class CallFunko{
-    constructor(endpoint)
+    constructor(api){
+        this.api = api
+    }
+    // My Index Route 
+    getFunkos(){
+        fetch(`${this.api}/contacts`)
+        .then(resp => resp.json)
+        .then(funkos => {
+            for (const funko of funkos){
+                
+            }
+        })
+    }
+   
 }

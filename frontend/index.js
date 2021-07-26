fa29fe76d181ab15b226d7608dbbda4703e4297e
Rene <renegarcia1864@gmail.com>
2021-07-26 16:10:23 -0500

    Fixed Collections

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,6 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-    CollectionServices.getCollection()
+    Collection.getCollection()
  })
 
 
--- frontend/src/collection.js
+++ frontend/src/collection.js
@@ -1,9 +1,27 @@
-class Collection {
+// Make the colelction calls and services 
+
+class Collection{
     static all = [];
-    
     constructor({name}){
-    this.name = name
-    Collection.all.push(this)
+        this.name = name
+        Collection.all.push(this)
     }
 
-}
+    // My Index Route 
+    static getCollection(){
+        fetch("http://localhost:3000/api/collections")
+        .then(resp => resp.json())
+        .then(data =>  Collection.renderCollection(data))
+    }
+    
+     
+    static renderCollection(data){
+        const titleName = document.querySelector("#intro")
+        data.forEach(collectionName => {
+            const h1 = document.createElement("h1")
+            console.log(h1)
+            h1.innerHTML = collectionName.name
+            titleName.append(h1)
+        });
+    }
+    }
